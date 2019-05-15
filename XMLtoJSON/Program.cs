fc081b191cc11965bb76e77bb25f51bb017d6a5e
Jakub <kuba.wasik.5a@gmail.com>
2019-05-15 15:59:36 +0200

    Dodaj pliki projektów.

---
+++ XMLtoJSON/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XMLtoJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
